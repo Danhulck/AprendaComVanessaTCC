3481cb0d88ecf7640a61dffdfc6abdc760a29703
Daniel jesus <familiaevd@yahoo.com.br>
2025-10-03 08:16:00 -0300

    carrossel pronto

--- src/app/componentes/categoria-receita/categoria-receita.component.ts
+++ src/app/componentes/categoria-receita/categoria-receita.component.ts
@@ -1,17 +1,24 @@
-import { Component, input } from '@angular/core';
+import { Component, input, ViewChild, ElementRef } from '@angular/core';
 import { CategoriaReceita } from '../receita-card/receita';
 import { ReceitaCardComponent } from '../receita-card/receita-card.component';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-categoria-receita',
-  imports: [
-    ReceitaCardComponent,
-    CommonModule
-  ],
+  imports: [ReceitaCardComponent, CommonModule],
   templateUrl: './categoria-receita.component.html',
   styleUrl: './categoria-receita.component.css'
 })
 export class CategoriaReceitaComponent {
   categoria = input.required<CategoriaReceita>();
+
+  @ViewChild('carousel', { static: false }) carousel!: ElementRef<HTMLDivElement>;
+
+  scrollLeft() {
+    this.carousel.nativeElement.scrollBy({ left: -300, behavior: 'smooth' });
+  }
+
+  scrollRight() {
+    this.carousel.nativeElement.scrollBy({ left: 300, behavior: 'smooth' });
+  }
 }
